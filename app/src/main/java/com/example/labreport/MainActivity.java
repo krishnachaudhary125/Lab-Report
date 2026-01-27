848c99fdb71683bbff509573fff2046106a03cbe
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-01-27 14:52:52 +0545

    wiring of all button

--- app/src/main/java/com/example/labreport/MainActivity.java
+++ app/src/main/java/com/example/labreport/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.labreport;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOne, btnTwo, btnThree, btnFour, btnFive, btnSix, btnSeven, btnEight, btnNine, btnTen, btnEleven, btnTwelve, btnThirteen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnOne = findViewById(R.id.one);
+        btnTwo = findViewById(R.id.two);
+        btnThree = findViewById(R.id.three);
+        btnFour = findViewById(R.id.four);
+        btnFive = findViewById(R.id.five);
+        btnSix = findViewById(R.id.six);
+        btnSeven = findViewById(R.id.seven);
+        btnEight = findViewById(R.id.eight);
+        btnNine = findViewById(R.id.nine);
+        btnTen = findViewById(R.id.ten);
+        btnEleven = findViewById(R.id.eleven);
+        btnTwelve = findViewById(R.id.twelve);
+        btnThirteen = findViewById(R.id.thirteen);
     }
 }
